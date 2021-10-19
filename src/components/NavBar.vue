5d6124a89d733bfbc027638a65fa8af5b63a7c8c
Samuel Matthew <samuelmatthew211@gmail.com>
2021-10-19 15:58:31 +0700

    :lipstick: Login Page UI done

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,11 +5,11 @@
       <router-link to="/"><h1>m t t h w .</h1></router-link>
     </div>
     <div class="d-flex">
-      <router-link to="/"><h5>home</h5></router-link>
-      <router-link to="/about"><h5>about</h5></router-link>
-      <router-link to="/keyboards"><h5>keyboards</h5></router-link>
-      <router-link to="/contact"><h5>contact</h5></router-link>
-      <router-link to="/login"><h5>sign in</h5></router-link>
+      <router-link to="/"><h6>home</h6></router-link>
+      <router-link to="/about"><h6>about</h6></router-link>
+      <router-link to="/keyboards"><h6>keyboards</h6></router-link>
+      <router-link to="/contact"><h6>contact</h6></router-link>
+      <router-link to="/login"><h6>log in</h6></router-link>
     </div>
   </nav>
 </template>
@@ -22,13 +22,20 @@ export default {
 
 <style>
   nav {
-    padding: 30px 100px;
+    padding: 15px 100px;
+    background-color: #26466a;
+    color: #fdf9f1;
   }
 
   nav .d-flex a {
     position: relative;
     margin-left: 20px;
     border-bottom: 2px solid rgba(0, 0, 0, 0);
+  }
+
+  nav .d-flex h6 {
+    font-family: 'Poppins', serif;
+    font-weight: 400;
   }
 
   nav .d-flex a:hover {
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,5 +1,20 @@
 <template>
-  <h1>hello</h1>
+  <section id="LoginPage" class="d-flex align-items-center justify-content-center">
+    <div class="card p-5" data-aos="fade-up" data-aos-duration="600">
+      <h1 class="mb-4">Log In</h1>
+      <label>E-mail</label>
+      <input type="email" name="email" class="form-control mt-1 mb-3">
+      <label>Password</label>
+      <input type="email" name="password" class="form-control mt-1 mb-3">
+      <input type="submit" name="email" class="form-control mt-3 py-2 btn-secondary" value="Log In">
+
+      <small class="text-muted text-center my-2">or</small>
+      <!-- google button -->
+      <input type="submit" name="email" class="form-control py-2 btn-secondary" value="Log In">
+
+      <small class="text-muted text-center mt-4">Don't have an account? Register <router-link to="/register" class="text-primary">here</router-link> </small>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -9,5 +24,11 @@ export default {
 </script>
 
 <style>
+  #LoginPage .card {
+    min-width: 500px;
+  }
 
+  #LoginPage input[type="submit"] {
+    background-color: #26466a;
+  }
 </style>
