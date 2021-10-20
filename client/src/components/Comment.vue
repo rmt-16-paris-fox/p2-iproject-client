31955e4d516be8d47dec9d5ad592b964b2e39d81
mrslnputra <mrslnputra@gmail.com>
2021-10-20 17:39:00 +0700

    finish adding card Post component to home, including its vuex function for getting all Post and comment

---
+++ client/src/components/Comment.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="d-flex flex-row mb-2">
+    <img :src="comment.User.imageUrl" width="40" class="rounded-image" />
+    <div class="d-flex flex-column ml-2 profile-comment">
+      <span class="name">{{ comment.User.fakeName }}</span>
+      <small class="comment-text">{{ comment.comment }}</small>
+      <div class="action">
+        <a href="">Edit</a> ||
+        <a href="">Delete</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Comment',
+  props: ['comment']
+}
+</script>
+
+<style scoped>
+.name {
+  margin-left: 10px;
+  margin-top: 10px;
+}
+.comment-text {
+  margin-left: 10px;
+  font-size: 15px;
+}
+
+.action{
+  position: absolute;
+  right: 10px;
+  bottom: 65px;
+}
+</style>
---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <h3>ChatNonymous</h3>
+      </div>
+      <ul class="nav ">
+        <li><router-link to="/" style="text-decoration: none; color: blue">Home</router-link></li>
+        <li><router-link to="/Profile" style="text-decoration: none; color: blue">Profile</router-link></li>
+        <li><router-link to="/Login" style="text-decoration: none; color: blue">Logout</router-link></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<style scopped>
+.navbar{
+  position: sticky;
+  top : 0;
+  z-index: 10000;
+}
+</style>
