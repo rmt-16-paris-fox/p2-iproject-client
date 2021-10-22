aaa6bbb5ccf97a17c8d2bdeb92158e3926584594
vinaekalaylani <vinaekalaylani@gmail.com>
2021-10-22 10:35:20 +0700

    set up delpoy

--- client/src/apis/server.js
+++ client/src/apis/server.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 
 export default axios.create({
-  baseURL: 'http://localhost:3000'
-  // baseURL: 'https://todoin-h8.herokuapp.com'
+  // baseURL: 'http://localhost:3000'
+  baseURL: 'https://todoin-h8.herokuapp.com'
 })
